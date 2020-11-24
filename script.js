f2177f3180aeefd45e38097f8eee2464fe32aee8
NakedSam <samuelst_pierre@hotmail.com>
2020-11-23 22:40:07 -0500

    Amélioré le validateur un tantinet

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const minSyllableField = document.getElementById("min-syllable-count")
 const maxSyllableField = document.getElementById("max-syllable-count")
 const wordCountField = document.getElementById("word-count")
 const vowels = ["a", "e", "i", "o", "u"]
-const consonants = ["b", "d", "f", "g", "j", "k", "m", "n", "p", "r", "s", "t", "v", "z", "ch", "sh", "ts"]
+const consonants = ["b", "d", "f", "g", "j", "k", "m", "n", "p", "r", "s", "t", "z", "ch", "sh", "ts"]
 const soundsToExclude = ["da", "ru", "ra", "gu", "ma", "sa", "tu", "ti", "te", "rim", "bim", "mos", "res",
  "eru", "oru", "bam", "zu", "piu", "pie", "su", "du", "pam", "nue", "foru", "tum", "ne", "ni", "no", "nu",
 "pa", "pi", "pe", "fa", "fi", "fe", "n"]
@@ -14,6 +14,51 @@ let newWords = []
 let syllableCount = 0
 const container = document.querySelector(".container")
 let wordsTable = document.getElementById("words-table")
+let soundsByLength = []
+let possibleSyllables = []
+let numberOfPossibilities = 0
+
+consonants.forEach( consonant => {
+    vowels.forEach( vowel => {
+        const syllable = consonant.concat(vowel)
+        possibleSyllables.push(syllable)
+    })
+})
+
+/*
+soundsToExclude.forEach( sound => {
+    soundArrayed = sound.split("")
+    soundLength = soundArrayed.length
+    //If there's not already something at index of sound length we create an array for it
+    //This will regroup all the sounds by their length
+    if (!soundsByLength[soundLength]) {
+        soundsByLength[soundLength] = []
+    }
+    soundsByLength[soundLength].push(sound)
+})
+*/
+function validateWordStructure(word) {
+    let isValid = false
+    //THIS WILL ONLY WORK IF THE MAXIMUM SIZE IN CHARACTERS FOR A CONSONANT OR VOWEL IS 2 !!!
+    const wordArrayed = word.split("")
+
+    /*
+    //We validate that the word follows the (V)CVCV... structure by removing
+    for(i = soundsByLength.length -1; i > 0; i--) {
+        soundsByLength[i].forEach( sound => {
+            let wordArrayed = word.split("")
+
+            //We split the words in chunks of letters by the length of the sound
+            for (y = 0; y < wordArrayed.length; y++) {
+                console.log(wordArrayed)
+            }
+    
+        })
+    }
+  */
+    return isValid
+}
+
 
 function generateWord(max, min, count) {  
     let stop = false
@@ -24,21 +69,40 @@ function generateWord(max, min, count) {
             const syllableCount = Math.round(Math.random() * (max - min) + min)
             newWord = ""
             for (y = 0; y < syllableCount; y++) {
-                //Decide if whether or not we should start with a vowel or not
-                let randomNumber = Math.random() * 2
 
-                if (randomNumber >= 1 && i === 0) {
+                //Decide if whether or not we should add an additional vowel
+                let randomNumber = Math.random() * 4
+            
+                if (randomNumber <= 1 && y < syllableCount) {
+                    //We generate a random number
                     randomNumber = Math.floor(Math.random() * vowels.length)
-                    newWord = newWord.concat(vowels[randomNumber])
+                    //If it's not the start of the word
+                    if (newWord.length > 0) {
+                        let vowelIsDifferent = false
+                        newWordArrayed = newWord.split("")
+                        //We check if the previous vowel is different 
+                        //(since double letters are impossible in Lorinin)
+                        while(!vowelIsDifferent) {
+                            //If the vowel are different, we generate a new number
+                            if (newWordArrayed[newWordArrayed.length - 1] === vowels[randomNumber]) {
+                                randomNumber = Math.floor(Math.random() * vowels.length)                  
+                            //Else we stop the loop
+                            } else {
+                                vowelIsDifferent = true
+                            }
+                            
+                        }
+                    }    
+                    //We add the vowel to the word and increase 
+                    newWord = newWord.concat(vowels[randomNumber])   
                     y ++
                 } 
+                
                 //If it's not the last loop
-                if (y < syllableCount && y < syllableCount) {
-                    //Add a random consonant to the word then a random vowel
-                    randomNumber = Math.floor(Math.random() * consonants.length)
-                    newWord = newWord.concat(consonants[randomNumber])
-                    randomNumber = Math.floor(Math.random() * vowels.length)
-                    newWord = newWord.concat(vowels[randomNumber])
+                if (y < syllableCount) {
+                    //Adds a random syllable
+                    randomNumber = Math.floor(Math.random() * possibleSyllables.length)
+                    newWord = newWord.concat(possibleSyllables[randomNumber])
                 }              
             }  
 
@@ -49,6 +113,9 @@ function generateWord(max, min, count) {
                     wordIsValid = false
                 }
             })
+
+            const structureIsValid = validateWordStructure(newWord)
+            
 
             //If the new word has already been generated we generate a new one else we push the word
             //into the array
@@ -69,13 +136,31 @@ function generateWord(max, min, count) {
     
 }
 
+function calculatePossibilities(min, max) {  
+    let possibilities = 0
+    let vcvcvPossibilities = 1     
+    let cvcvPossibilities = 1
+    let vcvvvcvvvPossibilities = 1
+    let cvvcvvPossibilities = 1
+
+    for (let i = min; i <= max; i++) {
+        //Check the VCVVCVV format
+        //Check the VCVCV format 
+        
+        //Check the format CVCV      
+        //Check the CVVCVV format
+    }
+
+}
+
 document.addEventListener("keydown", e => {
     if (e.key === "Enter") {
         //If the table is already filled, we empty it
         if (wordsTable.children.length !== 0) {
-
+            //MAYBE WORK ON THIS EVENTUALLY
         }
         //Make sure that number entered are actual numbers, if not give a default value
+        //(DOESNT SEEM TO WORK -_-)
         if (isNaN(minSyllableField.value || !minSyllableField.value)) {
             minSyllable = 1
         } else {
--- script_v.js
+++ script_v.js
@@ -1,28 +1,191 @@
 const wordToValidateField = document.getElementById("word-to-validate")
 const resultDiv = document.querySelector(".result")
 const soundsToExclude = ["da", "ru", "ra", "gu", "ma", "sa", "tu", "ti", "te", "rim", "bim", "mos", "res",
-"eru", "oru", "bam", "zu", "piu", "pie", "su", "du", "pam", "nue", "foru", "tum", "ne", "ni", "no", "nu",
+"eru", "oru", "bam", "zu", "piu", "pie", "su", "du", "pam", "nue", "tum", "ne", "ni", "no", "nu",
 "pa", "pi", "pe", "fa", "fi", "fe", "b", "d", "f", "g", "sh", "ch", "j", "k", "p", "r", "t", "v", "z", "n"]
 let wordToValidate = ""
-const excludedLetters = ["c", "l", "q", "x", "y"]
+const excludedLetters = ["c", "l", "q", "x", "y", "w"]
+let file = ""
+const vowels = ["a", "e", "i", "o", "u"]
+const consonants = ["b", "d", "f", "g", "j", "k", "m", "n", "p", "r", "s", "t", "z", "ch", "sh", "ts"]
+let newWords = []
+const wordsTable = document.getElementById("words-table")
+let linesArray = []
+let definitionsFile = ""
+let possibleSyllables = []
+let longestSyllableBlockLength = 0
+
+//We starts by adding every vowels to the array
+vowels.forEach( vowel => {
+    possibleSyllables.push(vowel)
+})
+
+//Then CV format
+//B A E
+consonants.forEach( consonant => {
+    vowels.forEach( vowel => {
+        let currentSyllable = consonant.concat(vowel)
+        possibleSyllables.push(currentSyllable)
+        //Then CVV Format
+        //E
+        vowels.forEach( vowelTwo => {
+            const initialCurrentSyllable = currentSyllable
+            //If the word doesnt end with the same vowel
+            if (!initialCurrentSyllable.endsWith(vowelTwo)) {
+                const syllableToAdd = initialCurrentSyllable.concat(vowelTwo)
+                possibleSyllables.push(syllableToAdd)
+                //Establish the longest block of letters' length
+                if (syllableToAdd.length > longestSyllableBlockLength) {
+                    longestSyllableBlockLength = syllableToAdd.length
+                }
+            }
+        })
+    })
+})
+
+function validateStructure(word) {
+    let isValid = true
+    let counter = 0
+    let stop = false
+    let isStartingWithVowel = false
+
+    while(!stop) {
+        //If the word starts with a vowel, we end the loop
+        if (word.startsWith(vowels[counter])) {
+            stop = true
+            isStartingWithVowel = true
+        }
+        //If the loop has not been stopped, we check if it's the end of the loop     
+        if (!stop) {
+            //If it's the last loop, we end the loop
+            if (counter === vowels.length - 1) {
+                stop = true
+            } else {
+                counter ++ 
+            }            
+        }   
+    }
+
+    counter = 0
+    wordArrayed = word.split("")
+    let lastChar = ""
+    stop = false
+    //We test for no repeating letter
+    while(!stop) {
+        if (counter === 0) {
+            lastChar = wordArrayed[0]
+        //If it's not the end of the loop
+        } else if (counter < wordArrayed.length) {
+            //If two characters are the same in a row, we exit the word and invalidates it
+            if (lastChar === wordArrayed[counter]) {
+                stop = true
+                isValid = false
+            } else {
+                lastChar = wordArrayed[counter]
+            }       
+        //Else (End of the loop), we stop the loop
+        } else {
+            stop = true
+        }
+       
+        counter ++
+    }
+
+    counter = 0
+
+    //If the word is still valid we keep going
+    if (isValid) {
+        //TODO REWRITE THIS VALIDATION
+        //We validate the pattern by replacing matching sequences from a word by a star
+        for (let i = longestSyllableBlockLength; i >= 0; i--) {
+            possibleSyllables.forEach( syllable => {
+                //So if the syllable has the same length as i (Loop goes from longest to shortest block)
+                //and the word contains the syllable, we replace it by asterisks
+                if (syllable.length === i && word.includes(syllable)) {
+                    word = word.replace(syllable, "*")
+                }
+            })
+        }     
+        //We check if the word follow the (v)cv(v)cv(v) pattern
+        //First if the word was starting with a vowel we remove the first character of the word
+        /*
+        if (isStartingWithVowel) {
+            wordArrayed.shift()
+        } else {
+            //HARD CODED TO SUPPORT VOWELS OF 1 IN LENGTH AND CONSONANTS UP TO 3
+            /*
+            if (wordArrayed.length < 4) {
+                const wordArrayedJoined = wordArrayed.join("")
+                wordArrayed = []
+                wordArrayed[0] = wordArrayedJoined
+            }
+            
+        }
+        */
+
+        //We check if the wordArrayed is 1 of lenght (Meaning less than four characters) or not
+
+    }
+
+    return isValid
+}
+
+function validateWord(word) { 
+    let isInvalid = false
+    let soundIsInvalid = false
+    let isContainingExcludedLetter = false
+    let isEndingWithVowel = false
+    let structureIsValid = false
+
+    soundsToExclude.forEach( sound => {
+        if (word.endsWith(sound)) {
+            soundIsInvalid = true
+        }
+    })
+
+    excludedLetters.forEach( letter => {
+        if (word.includes(letter)) {            
+            isContainingExcludedLetter = true
+        }
+    })
+
+    vowels.forEach( vowel => {
+        if (word.endsWith(vowel)) {
+            isEndingWithVowel = true
+        }
+    })
+
+    structureIsValid = validateStructure(word)
+
+    if (soundIsInvalid || isContainingExcludedLetter || !isEndingWithVowel || !structureIsValid) {
+        isInvalid = true
+    }
+
+    return isInvalid
+}
+
+function countSyllables(word) {
+    let numberOfSyllables = 0
+    let wordArrayed = word.split("")
+
+    wordArrayed.forEach( char => {
+        if (vowels.includes(char)) {
+            numberOfSyllables ++
+        }
+    })
+
+    return numberOfSyllables
+}
+
 
 document.addEventListener("keydown", e => {
     if (e.key === "Enter") {
         e.preventDefault()
+
         let isInvalid = false
 
         wordToValidate = wordToValidateField.value
-        soundsToExclude.forEach( sound => {
-            if (wordToValidate.endsWith(sound)) {
-                isInvalid = true
-            }
-        })
-
-        excludedLetters.forEach( letter => {
-            if (wordToValidate.includes(letter)) {            
-                isInvalid = true
-            }
-        })
+        isInvalid = validateWord(wordToValidate)
 
         if (isInvalid) {
             resultDiv.classList.remove("valid")
@@ -35,3 +198,153 @@ document.addEventListener("keydown", e => {
         }
     }
 })
+/*
+loadFileButton.addEventListener("click", e => {
+    fetch("old_words_list.txt")
+    .then(response => {
+        return response.text()
+    })
+    .then( myBlob => {
+        file = myBlob
+        fetch("definitions.txt")
+        .then( response => {
+            return response.text()
+        })
+        .then( myBlob => {
+            definitionsFile = myBlob
+            fileArrayed = file.split(/\r|\n/)
+            definitionsFileArrayed = definitionsFile.split(/\r|\n/)
+        
+            let lineCounter = 0
+
+            fileArrayed.forEach( line => {
+                line = line.trim()
+                const uploadedWordIsInvalid = validateWord(line)
+                const noOfSyllables = countSyllables(line)
+                linesArray[lineCounter] = line
+                
+        
+                if (!uploadedWordIsInvalid) {
+                    linesArray[lineCounter] = linesArray[lineCounter].concat("/" + definitionsFileArrayed[lineCounter])
+                } else {
+                    generateWord(noOfSyllables, noOfSyllables, 25)
+        
+                    let counter = 0
+                    let newRow = document.createElement("tr")
+                    let isHeaderWritten = false
+                    //We create the table
+                    newWords.forEach( word => {
+                        if (counter % 5 === 0) {
+                            wordsTable.appendChild(newRow)
+                            newRow = document.createElement("tr")
+                            counter = 0
+                        }
+                        
+                        if (!isHeaderWritten) {
+                            newTd = document.createElement("td")
+                            newTd.colSpan = "5"
+                            text = word + "/" + definitionsFileArrayed[lineCounter]
+                            newText = document.createTextNode(text)
+                            
+                            newTd.appendChild(newText)
+                            newRow.appendChild(newTd)
+                            wordsTable.appendChild(newRow)
+                            newTd = document.createElement("td") 
+                            newText = document.createTextNode(word)
+                            newTd.appendChild(newText)
+                            newRow.appendChild(newTd)
+                            isHeaderWritten = true
+                        } else {
+                            newTd = document.createElement("td") 
+                            newText = document.createTextNode(word)
+                            newTd.appendChild(newText)
+                            newRow.appendChild(newTd)
+                        }
+        
+                        counter ++
+                    })
+        
+                    wordsTable.appendChild(newRow)
+                    newWords = []
+                    isHeaderWritten = false
+                }
+        
+                
+                lineCounter ++
+            })
+        })
+    })
+/*
+    fetch("definitions.txt")
+    .then( response => {
+        return response.text()
+    })
+    .then( myBlob => {
+        definitionsFile = myBlob
+    })
+
+    fileArrayed = file.split(/\r|\n/)
+    definitionsFileArrayed = definitionsFile.split(/\r|\n/)
+
+    let lineCounter = 0
+
+    fileArrayed.forEach( line => {
+        line = line.trim()
+        const uploadedWordIsInvalid = validateWord(line)
+        const noOfSyllables = countSyllables(line)
+        linesArray[lineCounter] = line
+        
+
+        if (!uploadedWordIsInvalid) {
+            linesArray[lineCounter] = linesArray[lineCounter].concat("/" + definitionsFileArrayed[lineCounter])
+        } else {
+            generateWord(noOfSyllables, noOfSyllables, 25)
+
+            let counter = 0
+            let newRow = document.createElement("tr")
+            console.log(newRow)
+            let isHeaderWritten = false
+            //We create the table
+            newWords.forEach( word => {
+                if (counter % 5 === 0) {
+                    wordsTable.appendChild(newRow)
+                    newRow = document.createElement("tr")
+                    counter = 0
+                }
+                
+                if (!isHeaderWritten) {
+                    newTd = document.createElement("td")
+                    newTd.colSpan = "5"
+                    text = word + "/" + definitionsFileArrayed[lineCounter]
+                    newText = document.createTextNode(text)
+                    
+                    newTd.appendChild(newText)
+                    newRow.appendChild(newTd)
+                    wordsTable.appendChild(newRow)
+                    newTd = document.createElement("td") 
+                    newText = document.createTextNode(word)
+                    newTd.appendChild(newText)
+                    newRow.appendChild(newTd)
+                    isHeaderWritten = true
+                } else {
+                    newTd = document.createElement("td") 
+                    newText = document.createTextNode(word)
+                    newTd.appendChild(newText)
+                    newRow.appendChild(newTd)
+                }
+
+                counter ++
+            })
+
+            wordsTable.appendChild(newRow)
+            newWords = []
+            isHeaderWritten = false
+        }
+
+        
+        lineCounter ++
+    })
+*/
+/*
+})
+*/
